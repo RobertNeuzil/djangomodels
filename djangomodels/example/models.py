044e16349d138abd2b60e956561e8f9de24e5f5c
Robert Neuzil <robertneuzil@gmail.com>
2018-08-11 22:40:47 -0400

    many2man

--- djangomodels/example/models.py
+++ djangomodels/example/models.py
@@ -7,10 +7,17 @@ class Programmer (models.Model):
 	def __str__(self):
 		return self.name
 
-class Company (models.Model):
+class Language(models.Model):
 	name = models.CharField(max_length=20)
-	programmers = models.ForeignKey(Programmer, on_delete=models.CASCADE)
-
 
 	def __str__(self):
 		return self.name
+
+class Company (models.Model):
+	name = models.CharField(max_length=20)
+	programmers = models.ForeignKey(Programmer, on_delete=models.CASCADE)
+	languages = models.ManyToManyField(Language)
+
+	def __str__(self):
+		return self.name
+
