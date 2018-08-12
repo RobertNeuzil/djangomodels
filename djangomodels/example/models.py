27de494ac21c96df92de3de8d7c32a1792cff4ae
Robert Neuzil <robertneuzil@gmail.com>
2018-08-11 22:17:31 -0400

    didn't have foreign key

--- djangomodels/example/models.py
+++ djangomodels/example/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
-class Company (models.Model):
-	name = models.CharField(max_length=20)
+
 class Programmer (models.Model):
 	name = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.name
+
+class Company (models.Model):
+	name = models.CharField(max_length=20)
+	programmers = models.ForeignKey(Programmer, on_delete=models.CASCADE)
+
+
+	def __str__(self):
+		return self.name
