1f5aca15e693c3348123bcb578802b80bc6694c5
Robert Neuzil <robertneuzil@gmail.com>
2018-08-12 18:40:10 -0400

    more with model fields and sql queries

--- djangomodels/example/models.py
+++ djangomodels/example/models.py
@@ -1,23 +1,27 @@
 from django.db import models
 
+class Company(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    date_created = models.DateField() 
 
-class Programmer (models.Model):
-	name = models.CharField(max_length=20)
-
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 class Language(models.Model):
-	name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    creator = models.CharField(max_length=20)
+    paradigm = models.CharField(max_length=20)
+    date_created = models.DateField()
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-class Company (models.Model):
-	name = models.CharField(max_length=20)
-	programmers = models.ForeignKey(Programmer, on_delete=models.CASCADE)
-	languages = models.ManyToManyField(Language)
+class Programmer(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    languages = models.ManyToManyField(Language)
 
-	def __str__(self):
-		return self.name
-
+    def __str__(self):
+        return self.name
